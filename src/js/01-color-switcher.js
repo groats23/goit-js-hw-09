a86ba0b15dfd8a4384a05d57ab19660c28de2c39
groats23 <121118901+groats23@users.noreply.github.com>
2023-05-19 12:18:43 +0200

    css task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,4 @@
-
+const body = document.body;
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let bgColorChangeInterval = null;
