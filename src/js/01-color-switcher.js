cc712195ee96999e1dc4e57b51d9c036765e2d8b
groats23 <121118901+groats23@users.noreply.github.com>
2023-05-19 13:33:45 +0200

    task #1
    the end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,39 @@
+// Оголошуємо змінні
 const body = document.body;
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 let bgColorChangeInterval = null;
+
+// Додаємо обробники подій click до кнопок
+btnStart.addEventListener('click', onStartBtnClick);
+btnStop.addEventListener('click', onStopBtnClick);
+
+// Робимо неактивність кнопки Stop
+btnStop.setAttribute('disabled', true);
+
+// Задаэмо інтервал зміни кольора body
+function onStartBtnClick() {
+  bgColorChangeInterval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  // Робимо активність/неактивність кнопок Start/Stop
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+}
+
+// Зупинка зміни кольора фону
+function onStopBtnClick() {
+  clearInterval(bgColorChangeInterval);
+
+  // Робимо активність/неактивність кнопок Start/Stop
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+}
+
+// Генеруємо випадковий кольор
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
